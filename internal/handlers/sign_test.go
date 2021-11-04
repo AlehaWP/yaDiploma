49f48049d34a95b3f2db22431447740327e27c90
AlehaWP <nerchenko@gmail.com>
2021-11-04 21:41:51 +0300

    Добавил тестового клиента

--- internal/handlers/sign_test.go
+++ internal/handlers/sign_test.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"net/http"
+	"reflect"
 	"testing"
+
+	"github.com/AlehaWP/yaDiploma.git/internal/models"
 )
 
 func TestHandlerLogin(t *testing.T) {
@@ -20,4 +24,24 @@ func TestHandlerLogin(t *testing.T) {
 	// 		HandlerLogin(tt.args.w, tt.args.r)
 	// 	})
 	// }
+}
+
+func TestHandlerRegistration(t *testing.T) {
+	type args struct {
+		ur models.UsersRepo
+	}
+	tests := []struct {
+		name string
+		args args
+		want http.HandlerFunc
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HandlerRegistration(tt.args.ur); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("HandlerRegistration() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
