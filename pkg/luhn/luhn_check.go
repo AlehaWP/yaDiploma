40ef8870fa093f8876ce352a399eb5932ee39bba
AlehaWP <nerchenko@gmail.com>
2021-11-05 20:29:22 +0300

    Добавил проверку по алгоритму Луна инта

---
+++ pkg/luhn/luhn_check.go
@@ -0,0 +1,48 @@
+package luhn
+
+import (
+	"strconv"
+
+	"github.com/AlehaWP/yaDiploma.git/pkg/logger"
+)
+
+func CheckInteger(i int) bool {
+	str := strconv.Itoa(i)
+	return CheckString(str)
+}
+
+func CheckString(s string) bool {
+	var rArr []int
+	for _, v := range s {
+		// Десятичные значения
+		res, err := strconv.Atoi(string(v))
+		if err != nil {
+			logger.Info("Luhn", "Ошибка конвертации строки", err)
+		}
+		rArr = append(rArr, res)
+	}
+	return Check(rArr)
+}
+
+func Check(arr []int) bool {
+	l := len(arr)
+	controlSum := arr[l-1]
+	cArr := arr[:l-1]
+	var cd int
+	for i, v := range cArr {
+		if i%2 == 0 {
+			v *= 2
+			if v > 9 {
+				v -= 9
+			}
+		}
+		cd += v
+	}
+
+	if (cd+controlSum)%10 != 0 {
+		return false
+	}
+
+	return true
+
+}
