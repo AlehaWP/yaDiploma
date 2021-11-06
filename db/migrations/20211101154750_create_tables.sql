c0003919d34a215b18c37edf755fdc717cc3cba0
AlehaWP <nerchenko@gmail.com>
2021-11-06 20:02:43 +0300

    Изменил таблицы по движению средств

--- db/migrations/20211101154750_create_tables.sql
+++ db/migrations/20211101154750_create_tables.sql
@@ -8,12 +8,6 @@ CREATE TABLE IF NOT EXISTS users (
         user_token VARCHAR(36),
 		date_add TIMESTAMPTZ(0) default (NOW() at time zone 'UTC+3'));
 
-CREATE TABLE IF NOT EXISTS customers (
-        id SERIAL PRIMARY KEY,
-        user_id INT UNIQUE,
-        balance NUMERIC,
-		date_add TIMESTAMPTZ(0) default (NOW() at time zone 'UTC+3'));
-
 CREATE TABLE IF NOT EXISTS orders (
 		id SERIAL PRIMARY KEY,
 		user_id INT NOT NULL,
@@ -26,8 +20,15 @@ CREATE TABLE IF NOT EXISTS balance_log (
         id SERIAL PRIMARY KEY,
         user_id INT NOT NULL,
         order_id VARCHAR(50),
-        summ NUMERIC,
+        sum_in NUMERIC default 0,
+        sum_out NUMERIC default 0, 
         date_add TIMESTAMPTZ(0) default (NOW() at time zone 'UTC+3'));
+
+
+CREATE OR REPLACE VIEW customers AS
+        select bl.user_id, sum(bl.sum_in - bl.sum_out) as current_balance, 
+        MAX(bl.date_add) as last_event_date from balance_log bl 
+        group by bl.user_id;
 
 -- +goose StatementEnd
 
--- internal/database/tables.go
+++ internal/database/tables.go
@@ -25,15 +25,6 @@ func (s *serverDB) createTables(ctx context.Context) {
 		logger.Panic("Ошибка создания таблиц", err)
 	}
 
-	_, err = tx.ExecContext(ctx, `CREATE TABLE IF NOT EXISTS customers (
-									id SERIAL PRIMARY KEY,
-									user_id INT UNIQUE,
-									balance NUMERIC default 0,
-									date_add TIMESTAMPTZ(0) default (NOW() at time zone 'UTC+3'))
-	`)
-	if err != nil {
-		logger.Panic("Ошибка создания таблиц", err)
-	}
 	_, err = tx.ExecContext(ctx, `CREATE TABLE IF NOT EXISTS orders (
 									id SERIAL PRIMARY KEY,
 									user_id INT NOT NULL,
@@ -49,8 +40,18 @@ func (s *serverDB) createTables(ctx context.Context) {
 									id SERIAL PRIMARY KEY,
 									user_id INT NOT NULL,
 									order_id VARCHAR(50),
-									accrual NUMERIC,
+									sun_in NUMERIC default 0,
+									sum_out NUMERIC default 0, 
 									date_add TIMESTAMPTZ(0) default (NOW() at time zone 'UTC+3'))
+	`)
+	if err != nil {
+		logger.Panic("Ошибка создания таблиц", err)
+	}
+
+	_, err = tx.ExecContext(ctx, `CREATE OR REPLACE VIEW customers AS
+									select bl.user_id, sum(bl.sum_in - bl.sum_out) as current_balance, 
+									MAX(bl.date_add) as last_event_date from balance_log bl 
+									group by bl.user_id
 	`)
 	if err != nil {
 		logger.Panic("Ошибка создания таблиц", err)
